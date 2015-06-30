d67179bd1fb21127efafe4e47f48d8329a40ca44
Istvan Kiesz <kieszistvan@gmail.com>
2015-06-30 18:08:39 +0200

    slacko jacko

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var _ = require('lodash');
+var async = require('async');
 var evcheck = require('evcheck');
 var morgan = require('morgan');
 var express = require('express');
 var winston = require('winston');
 var Slack = require('slack-node');
 var metaReplacer = require('./lib/meta-replacer');
+
+var redis = require('redis');
+var mongoose = require('mongoose');
 
 var logger = new(winston.Logger)({
     transports: [
@@ -14,61 +18,111 @@ var logger = new(winston.Logger)({
     ]
 });
 
-var config = {
-    port: process.env.PORT,
-    slackToken: process.env.SLACK_TOKEN,
-    fetchInterval: process.env.INTERVAL
-};
-
-evcheck.checkVars(['PORT', 'INTERVAL', 'SLACK_TOKEN'], function(err) {
+evcheck.checkVars(['PORT'], function(err) {
     if (err) {
-        console.log(err.message);
+        logger.error(err.message);
         process.exit(9);
     }
 });
 
-var slack = new Slack(config.slackToken);
+var config = {
+    port: process.env.PORT,
+    fetchInterval: process.env.INTERVAL || 30000,
+    mongo: {
+        host: process.env.MONGO_HOST || '192.168.59.103',
+        port: process.env.MONGO_PORT || 27017,
+        name: process.env.MONGO_NAME || 'platform'
+    },
+    redis: {
+        host: process.env.REDIS_HOST || '192.168.59.103',
+        port: process.env.REDIS_PORT || 6379
+    }
+};
 
-var usersMap = [];
-var channelsMap = [];
+
+var redisClient = redis.createClient(config.redis.port, config.redis.host, {});
+var mongooseClient = mongoose.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.name);
+
+mongooseClient.connection.on('error', function(error) {
+    logger.error(error);
+});
+
+logger.info('Mongo services at ' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.name);
+
+global.config = config;
+global.mongoose = mongooseClient;
+global.redis = redisClient;
+global.logger = logger;
+
+var SlackTeam = require('./lib/SlackTeam');
+
+var REDIS_SLACK_PREFIX = "slackteam:";
+var SLACK_API = {
+    USERS_LIST: 'users.list',
+    CHANNELS_LIST: 'channels.list',
+    CHANNELS_HISTORY: 'channels.history'
+};
 
 function fetchSlackInfo() {
+
     process.nextTick(function() {
-        slack.api('users.list', {}, function(err, data) {
-            if (err || !data.ok) {
-                logger.error(err || data);
+
+        var findAllSlackTeamsResult = function findAllSlackTeamsResult(err, teams) {
+            if (err) {
+                logger.error(err);
                 return;
             }
 
-            logger.info('Fetched %d users', data.members.length);
+            logger.info('Found %d teams', teams.length);
 
-            data.members.forEach(function(member) {
-                usersMap[member.id] = {
-                    username: member.name,
-                    firstName: member.profile.first_name,
-                    lastName: member.profile.last_name,
-                    pictureUrl: member.profile.image_72
+            var createTeamSlack = function createTeamSlack(team, slackApi, dataProperty, callback) {
+                if (!team.token) {
+                    var errMsg = 'Cannot fetch ' + slackApi + ' of ' + team.name + '(noslacktoken)';
+                    logger.error(errMsg);
+                    return callback(errMsg);
+                }
+
+                logger.debug('(%s) API client created for %s', slackApi, team.name);
+                callback(null, team, slackApi, dataProperty, new Slack(team.token));
+            };
+
+            var fetchSlackApi = function fetchSlackApi(team, slackApi, dataProperty, slackClient, callback) {
+                slackClient.api(slackApi, {}, function(err, data) {
+                    var error = (err || !data.ok) ? err || data : '';
+                    if (error) {
+                        logger.error(error);
+                        return callback(error);
+                    }
+
+                    logger.info('Fetched %d %s', data[dataProperty].length, slackApi);
+                    callback(null, team, dataProperty, data);
+                });
+            };
+
+            var storeInRedis = function storeInRedis(team, dataProperty, data, callback) {
+                data[dataProperty].forEach(function(entity) {
+                    redisClient.hset(REDIS_SLACK_PREFIX + team.name, entity.id, JSON.stringify(entity));
+                });
+            };
+
+            var fetchAndStore = async.seq(createTeamSlack, fetchSlackApi, storeInRedis);
+
+            var scheduleFetch = function scheduleFetch(slackApi, dataProperty) {
+                return function(team) {
+                    setTimeout(function() {
+                        fetchAndStore(team, slackApi, dataProperty);
+                    }, 1000);
                 };
-            });
-        });
-        slack.api('channels.list', {}, function(err, data) {
-            if (err || !data.ok) {
-                logger.error(err || data);
-                return;
-            }
+            };
 
-            logger.info('Fetched %d channels', data.channels.length);
+            teams.forEach(scheduleFetch(SLACK_API.USERS_LIST, 'members'));
+            teams.forEach(scheduleFetch(SLACK_API.CHANNELS_LIST, 'channels'));
+        };
 
-            data.channels.forEach(function(channel) {
-                channelsMap[channel.id] = {
-                    name: channel.name
-                };
-            });
-        });
-
-
-        setTimeout(fetchSlackInfo, config.fetchInterval);
+        SlackTeam.find({}, findAllSlackTeamsResult);
     });
+
+    setTimeout(fetchSlackInfo, config.fetchInterval);
 }
 
 fetchSlackInfo();
@@ -88,35 +142,78 @@ app.use(function setNoCacheHeaders(req, res, next) {
     next();
 });
 
-app.get('/api/v1/messages/:channelId',
+app.get('/api/:team/:channelId/messages',
     function(req, res, next) {
-        slack.api('channels.history', {
-            channel: req.params.channelId
-        }, function(err, data) {
-            if (err || !data.ok) {
-                logger.info(err || data);
-                res.status(500).json(err || data);
-                return;
+        var team = req.params.team;
+        var channelId = req.params.channelId;
+
+        function convertFromUser(ids, userId) {
+            var memberString = ids[userId];
+
+            if (memberString) {
+                var member = JSON.parse(memberString);
+                return {
+                    username: member.name,
+                    firstName: member.profile.first_name,
+                    lastName: member.profile.last_name,
+                    pictureUrl: member.profile.image_72
+                };
+            } else {
+                return null;
+            }
+        }
+
+        logger.info('Fetch channel(%s) from %s', channelId, team);
+
+        SlackTeam.findOne({
+            name: team
+        }, function(err, slackTeam) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).json(err);
             }
 
-            var transformedMessages = _.reduce(data.messages, function(messages, message) {
-                var date = parseFloat(message.ts.split('.').shift()) * 1000;
-
-                if (message.type === 'message') {
-                    var replacedMessage = metaReplacer(usersMap, channelsMap, message.text);
-                    messages.push({
-                        user: usersMap[message.user],
-                        text: replacedMessage,
-                        date: new Date(date)
-                    });
+            if (!slackTeam) {
+                return res.status(404).json({
+                    message: 'No Slack team domain found with this ID'
+                });
+            }
+            var slackClient = new Slack(slackTeam.token);
+            slackClient.api(SLACK_API.CHANNELS_HISTORY, {
+                channel: channelId
+            }, function(err, data) {
+                if (err || !data.ok) {
+                    logger.info(err || data);
+                    res.status(500).json(err || data);
+                    return;
                 }
 
-                return messages;
-            }, []);
+                redisClient.hgetall(REDIS_SLACK_PREFIX + team, function(err, ids) {
+                    var teamInfo = ids[channelId];
+                    logger.info('Channel resolved to %s', JSON.parse(teamInfo).name);
 
-            res.set('Content-type', 'application/json');
-            res.json(transformedMessages);
+                    logger.info(data.messages)
+                    var transformedMessages = _.reduce(data.messages, function(messages, message) {
+                        var date = parseFloat(message.ts.split('.').shift()) * 1000;
+
+                        if (message.type === 'message') {
+                            var replacedMessage = metaReplacer(ids, message.text);
+                            messages.push({
+                                user: convertFromUser(ids, message.user),
+                                text: replacedMessage,
+                                date: new Date(date)
+                            });
+                        }
+
+                        return messages;
+                    }, []);
+
+                    res.set('Content-type', 'application/json');
+                    res.json(transformedMessages);
+                });
+            });
         });
+
     });
 
 app.get('/healthcheck',
